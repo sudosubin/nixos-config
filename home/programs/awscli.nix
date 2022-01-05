cf351710b0080ca5c3af51e8aa5be2d92b9cb53b
sudosubin <sudosubin@gmail.com>
2022-01-05 11:02:52 +0000

    feat: Add aws-vault configurations

--- home/programs/awscli.nix
+++ home/programs/awscli.nix
@@ -2,8 +2,16 @@
 
 {
   home.packages = with pkgs; [
+    aws-vault
     awscli2
   ];
+
+  home.sessionVariables = {
+    AWS_VAULT_BACKEND = "pass";
+    AWS_VAULT_PROMPT = "pass";
+    AWS_VAULT_PASS_PREFIX = "aws-vault";
+    PASS_OATH_CREDENTIAL_NAME = "aws/mathpresso";
+  };
 
   home.file = {
     ".aws/config".text = ''
