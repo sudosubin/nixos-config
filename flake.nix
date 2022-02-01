680b8e8caf1e243254a8fb87a2a393d00079938c
sudosubin <sudosubin@gmail.com>
2022-02-01 13:58:31 +0900

    feat: Add nixos-config-private modules

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       url = "github:nixos/nixpkgs/nixpkgs-unstable";
     };
 
-    darwin = {
+    nix-darwin = {
       url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -21,17 +21,29 @@
       # url = "path:/home/sudosubin/Code/sudosubin/home-manager-secrets";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixos-config-private-sudosubin = {
+      url = "git+ssh://git@github.com/sudosubin/nixos-config-private?ref=main&dir=sudosubin";
+      # url = "path:/home/sudosubin/Code/sudosubin/nixos-config-private/sudosubin";
+    };
+
+    nixos-config-private-toss = {
+      url = "git+ssh://git@github.com/sudosubin/nixos-config-private?ref=main&dir=toss";
+      # url = "path:/home/sudosubin/Code/sudosubin/nixos-config-private/toss";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    darwin,
+    nix-darwin,
     home-manager,
     home-manager-secrets,
+    nixos-config-private-sudosubin,
+    nixos-config-private-toss,
     ...
   }: {
-    nixosConfigurations.darwin = darwin.lib.darwinSystem {
+    nixosConfigurations.darwin = nix-darwin.lib.darwinSystem {
       system = "x86_64-darwin";
       modules = [
         home-manager.darwinModules.home-manager
@@ -45,6 +57,8 @@
         {
           home-manager.users.sudosubin.imports = [
             home-manager-secrets.homeManagerModules.home-manager-secrets
+            nixos-config-private-sudosubin.homeManagerModules.sudosubin
+            nixos-config-private-toss.homeManagerModules.toss
           ];
         }
         ./modules/linux/configuration.nix
