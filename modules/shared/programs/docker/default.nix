3909eb0114362285dd4e74c8a3d22f6e2bf90310
sudosubin <sudosubin@gmail.com>
2022-02-23 15:31:04 +0900

    feat: Add colima package for macos

--- modules/shared/programs/docker/default.nix
+++ modules/shared/programs/docker/default.nix
@@ -1,8 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  inherit (pkgs) stdenv;
+
+in
 {
   home.packages = with pkgs; [
     docker
     docker-compose_2
-  ];
+  ] ++ lib.optionals stdenv.isDarwin [ colima ];
 }
