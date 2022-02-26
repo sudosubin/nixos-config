00e4c3822405772fdc926bc08e0afc35f17fda8a
sudosubin <sudosubin@gmail.com>
2022-02-26 15:53:01 +0900

    chore: Add tmux vim binding

--- modules/shared/programs/tmux/default.nix
+++ modules/shared/programs/tmux/default.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    tmux
-  ];
-
   programs.tmux = {
     enable = true;
     terminal = "screen-256color";
@@ -13,6 +9,12 @@
 
       # ctrl+b highlight
       set -g status-right '#{?client_prefix,#[reverse] ^B #[noreverse],} | %a %Y-%m-%d %H:%M'
+
+      # vim binding
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
 
       # open new window/pane from current path
       bind '"' split-window -c "#{pane_current_path}"
