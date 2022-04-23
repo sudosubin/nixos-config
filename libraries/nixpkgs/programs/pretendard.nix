17df6d7ecc660a23fda9dfd8da36804bcf6bdf52
sudosubin <sudosubin@gmail.com>
2022-04-23 22:26:46 +0900

    feat: Version up pretendard

--- libraries/nixpkgs/programs/pretendard.nix
+++ libraries/nixpkgs/programs/pretendard.nix
@@ -1,14 +1,14 @@
 final: { lib, fetchzip, ... }@prev:
 
 let
-  version = "1.2.1";
+  version = "1.3.0";
 in
 {
   pretendard = fetchzip {
     name = "pretendard-${version}";
 
     url = "https://github.com/orioncactus/pretendard/releases/download/v${version}/Pretendard-${version}.zip";
-    sha256 = "sha256-tqOKiSwQRx4GYnGK0hlQGk5kUEfXgHv/d4IEG5qleFo=";
+    sha256 = "sha256-tjBe2mDZmk0vo4axDlbGOboUPqrA5hQi4tKC4KoZrVo=";
 
     postFetch = ''
       mkdir -p $out/share/fonts/opentype/pretendard
