c3198d99cd7210931151048d0b287c8334270d1e
sudosubin <sudosubin@gmail.com>
2022-05-17 00:32:45 +0900

    feat(pretendard): Update pretendard library

--- libraries/nixpkgs/programs/pretendard/default.nix
+++ libraries/nixpkgs/programs/pretendard/default.nix
@@ -1,26 +1,46 @@
-final: { lib, fetchzip, ... }@prev:
+final: { lib, stdenv, fetchzip, ... }@prev:
 
 let
   version = "1.3.0";
+
+  mkPretendard = { pname, typeface, sha256 }:
+    fetchzip {
+      name = "${pname}-${version}";
+
+      url = "https://github.com/orioncactus/pretendard/releases/download/v${version}/${typeface}-${version}.zip";
+      inherit sha256;
+
+      postFetch = ''
+        mkdir -p $out/share/fonts/opentype/${typeface}
+        unzip -j $downloadedFile "*.otf" -d $out/share/fonts/opentype/${typeface}/
+      '';
+
+      meta = with lib; {
+        homepage = "https://github.com/orioncactus/pretendard";
+        description = "Pretendard Typeface";
+        license = licenses.ofl;
+        platforms = platforms.all;
+        maintainers = with maintainers; [ sudosubin ];
+      };
+    };
+
 in
 {
-  pretendard = fetchzip {
-    name = "pretendard-${version}";
+  pretendard = mkPretendard {
+    pname = "pretendard";
+    typeface = "Pretendard";
+    sha256 = "sha256-w37Yc4vDbzLJWlP01WhWefQKvx/54sBBMYZZRJ7btg8=";
+  };
 
-    url = "https://github.com/orioncactus/pretendard/releases/download/v${version}/Pretendard-${version}.zip";
-    sha256 = "sha256-tjBe2mDZmk0vo4axDlbGOboUPqrA5hQi4tKC4KoZrVo=";
+  pretendard-jp = mkPretendard {
+    pname = "pretendard-jp";
+    typeface = "PretendardJp";
+    sha256 = "sha256-beCUQmF57kqwxHwOvQhmr+dcoZHvMFOcujOD+ZJUqTo=";
+  };
 
-    postFetch = ''
-      mkdir -p $out/share/fonts/opentype/pretendard
-      unzip -j $downloadedFile "*.otf" -d $out/share/fonts/opentype/pretendard/
-    '';
-
-    meta = with lib; {
-      homepage = "https://github.com/orioncactus/pretendard";
-      description = "Pretendard Typeface";
-      license = licenses.ofl;
-      platforms = platforms.all;
-      maintainers = [ maintainers.sudosubin ];
-    };
+  pretendard-std = mkPretendard {
+    pname = "pretendard-std";
+    typeface = "PretendardStd";
+    sha256 = "sha256-cHStOwsMLmGmyPHdyNPjcMnXgrBGBIGoWeavDxY4lSw=";
   };
 }
