02de7f8ed32ea49cdc20b37998e2463dd1f6c266
sudosubin <sudosubin@gmail.com>
2022-06-18 21:41:37 +0900

    feat: Add aws-vault session variables

--- modules/shared/programs/aws/default.nix
+++ modules/shared/programs/aws/default.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  home.sessionVariables = {
+    AWS_VAULT_BACKEND = "file";
+    AWS_VAULT_PROMPT = "1password";
+    AWS_VAULT_FILE_PASSPHRASE = "";
+  };
+
   home.packages = with pkgs; [
     awscli2
     aws-vault
