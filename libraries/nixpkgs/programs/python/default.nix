5904a507808ebb4e4c54c6b35d521ef5cf2a47e1
sudosubin <sudosubin@gmail.com>
2022-06-30 23:53:54 +0900

    feat: Overlay python310Packages.fonttools for darwin

--- libraries/nixpkgs/programs/python/default.nix
+++ libraries/nixpkgs/programs/python/default.nix
@@ -3,6 +3,7 @@ final: { lib, fetchzip, stdenv, ... }@prev:
 {
   python310 = prev.python310.override {
     packageOverrides = self: super: {
+      inherit (import ./fonttools self super) fonttools;
       inherit (import ./pyopenssl self super) pyopenssl;
     };
   };
