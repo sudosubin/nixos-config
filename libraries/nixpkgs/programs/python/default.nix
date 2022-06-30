2a326b976245a63d6117d394a9070cce9bbd5c04
Subin Kim <32478597+sudosubin@users.noreply.github.com>
2022-07-01 00:57:54 +0900

    feat: Version up (#35)
    * feat: Version up nix flake lock
    
    * fix: Fix darwin build patch for python packages
    
    * feat: Remove uharfbuzz overlay
    
    * feat: Remove skia-pathops overlay
    
    * feat: Overlay python310Packages.fonttools for darwin

--- libraries/nixpkgs/programs/python/default.nix
+++ libraries/nixpkgs/programs/python/default.nix
@@ -1,8 +1,9 @@
 final: { lib, fetchzip, stdenv, ... }@prev:
 
 {
-  python39 = prev.python39.override {
+  python310 = prev.python310.override {
     packageOverrides = self: super: {
+      inherit (import ./fonttools self super) fonttools;
       inherit (import ./pyopenssl self super) pyopenssl;
     };
   };
