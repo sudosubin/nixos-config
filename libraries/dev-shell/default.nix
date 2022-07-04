54a8ad4ce0200f8d7f78f6ff786a088620c1d199
Subin Kim <32478597+sudosubin@users.noreply.github.com>
2022-07-05 02:32:49 +0900

    feat(pipeline): Optimize pipelines (#37)
    * feat(pipeline): Change to use current flake nixpkgs-fmt
    
    * feat(pipeline): Update deps pipeline

--- libraries/dev-shell/default.nix
+++ libraries/dev-shell/default.nix
@@ -2,21 +2,26 @@
 with inputs;
 
 flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
+let
+  pkgs = nixpkgs.legacyPackages.${system};
 
-  in
-  {
-    checks = {
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ../.;
-        hooks = {
-          nixpkgs-fmt.enable = true;
-        };
+in
+{
+  apps = {
+    format = flake-utils.lib.mkApp {
+      drv = pkgs.nixpkgs-fmt;
+    };
+  };
+  checks = {
+    pre-commit-check = pre-commit-hooks.lib.${system}.run {
+      src = ../.;
+      hooks = {
+        nixpkgs-fmt.enable = true;
       };
     };
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [ pre-commit ];
-      inherit (self.checks.${system}.pre-commit-check) shellHook;
-    };
-  })
+  };
+  devShell = pkgs.mkShell {
+    buildInputs = with pkgs; [ pre-commit ];
+    inherit (self.checks.${system}.pre-commit-check) shellHook;
+  };
+})
