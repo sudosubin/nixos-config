5c66f2705392f3c9c05423ff7573e035303bf74b
sudosubin <sudosubin@gmail.com>
2022-07-08 20:35:40 +0900

    feat: Add darwin system.defaults configurations

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -23,11 +23,32 @@
   };
 
   system.defaults = {
+    # dock configurations
     dock.autohide = true;
+    dock.mineffect = "suck";
     dock.mru-spaces = false;
+    dock.showhidden = false;
+    dock.show-recents = false;
+    dock.tilesize = 36;
+
+    # finder configurations
+    finder.AppleShowAllFiles = true;
+    finder.AppleShowAllExtensions = true;
+
+    # system configurations
+    NSGlobalDomain.AppleShowAllFiles = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = false;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+
+    NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+    NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+    NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+    NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+    NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+
     NSGlobalDomain.InitialKeyRepeat = 15;
     NSGlobalDomain.KeyRepeat = 2;
-
     NSGlobalDomain.NSWindowResizeTime = null;
     NSGlobalDomain.NSAutomaticWindowAnimationsEnabled = false;
   };
