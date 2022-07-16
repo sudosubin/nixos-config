6c2049103b3e489362c2ad96c57e68d0b32f6416
sudosubin <sudosubin@gmail.com>
2022-07-16 20:17:22 +0900

    feat: Refactor sway configurations

--- modules/linux/programs/wayland/sway.nix
+++ modules/linux/programs/wayland/sway.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let
-  mod = config.wayland.windowManager.sway.config.modifier;
+  sway = config.wayland.windowManager.sway.package;
   left = "h";
   right = "l";
   up = "k";
@@ -11,12 +11,12 @@ in
 {
   home.packages = with pkgs; [
     polkit_gnome
-    sway
   ];
 
   wayland.windowManager.sway = {
     enable = true;
-    config = {
+    wrapperFeatures.gtk = true;
+    config = rec {
       modifier = "Mod4";
       terminal = "${pkgs.alacritty}/bin/alacritty";
       fonts = {
@@ -37,17 +37,17 @@ in
       };
       bars = [ ];
       startup = [
-        { command = "${pkgs.sway}/bin/swaymsg workspace 1"; }
+        { command = "${sway}/bin/swaymsg workspace 1"; }
         { command = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"; }
       ];
       keybindings = {
         # Core sway
-        "${mod}+Shift+c" = "reload";
-        "${mod}+Shift+r" = "restart";
-        "${mod}+Shift+e" = "exit";
+        "${modifier}+Shift+c" = "reload";
+        "${modifier}+Shift+r" = "restart";
+        "${modifier}+Shift+e" = "exit";
 
         # Terminal
-        "${mod}+Return" = "exec ${config.wayland.windowManager.sway.config.terminal}";
+        "${modifier}+Return" = "exec ${config.wayland.windowManager.sway.config.terminal}";
         "Control+Mod1+t" = "exec ${config.wayland.windowManager.sway.config.terminal}";
 
         # Kill window
@@ -57,59 +57,59 @@ in
         "Mod1+space" = "exec ${pkgs.rofi}/bin/rofi -show drun";
 
         # Focus window
-        "${mod}+${left}" = "focus left";
-        "${mod}+${down}" = "focus down";
-        "${mod}+${up}" = "focus up";
-        "${mod}+${right}" = "focus right";
-        "${mod}+space" = "focus mode_toggle";
-        "${mod}+Shift+space" = "floating toggle";
+        "${modifier}+${left}" = "focus left";
+        "${modifier}+${down}" = "focus down";
+        "${modifier}+${up}" = "focus up";
+        "${modifier}+${right}" = "focus right";
+        "${modifier}+space" = "focus mode_toggle";
+        "${modifier}+Shift+space" = "floating toggle";
 
         # Move window
-        "${mod}+Shift+${left}" = "move left";
-        "${mod}+Shift+${down}" = "move down";
-        "${mod}+Shift+${up}" = "move up";
-        "${mod}+Shift+${right}" = "move right";
+        "${modifier}+Shift+${left}" = "move left";
+        "${modifier}+Shift+${down}" = "move down";
+        "${modifier}+Shift+${up}" = "move up";
+        "${modifier}+Shift+${right}" = "move right";
 
         # Switch workspace
-        "${mod}+1" = "workspace number 1";
-        "${mod}+2" = "workspace number 2";
-        "${mod}+3" = "workspace number 3";
-        "${mod}+4" = "workspace number 4";
-        "${mod}+5" = "workspace number 5";
-        "${mod}+6" = "workspace number 6";
-        "${mod}+7" = "workspace number 7";
-        "${mod}+8" = "workspace number 8";
-        "${mod}+9" = "workspace number 9";
-        "${mod}+0" = "workspace number 10";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
 
         # Move to workspace
-        "${mod}+Shift+1" = "move container to workspace number 1";
-        "${mod}+Shift+2" = "move container to workspace number 2";
-        "${mod}+Shift+3" = "move container to workspace number 3";
-        "${mod}+Shift+4" = "move container to workspace number 4";
-        "${mod}+Shift+5" = "move container to workspace number 5";
-        "${mod}+Shift+6" = "move container to workspace number 6";
-        "${mod}+Shift+7" = "move container to workspace number 7";
-        "${mod}+Shift+8" = "move container to workspace number 8";
-        "${mod}+Shift+9" = "move container to workspace number 9";
-        "${mod}+Shift+0" = "move container to workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
 
         # Split, Layout
-        "${mod}+b" = "splith";
-        "${mod}+v" = "splitv";
-        "${mod}+s" = "layout stacking";
-        "${mod}+w" = "layout tabbed";
-        "${mod}+e" = "layout toggle split";
+        "${modifier}+b" = "splith";
+        "${modifier}+v" = "splitv";
+        "${modifier}+s" = "layout stacking";
+        "${modifier}+w" = "layout tabbed";
+        "${modifier}+e" = "layout toggle split";
 
         # Fullscreen
-        "${mod}+f" = "fullscreen toggle";
+        "${modifier}+f" = "fullscreen toggle";
 
         # Scratchpad
-        "${mod}+Shift+minus" = "move scratchpad";
-        "${mod}+minus" = "scratchpad show";
+        "${modifier}+Shift+minus" = "move scratchpad";
+        "${modifier}+minus" = "scratchpad show";
 
         # Modes
-        "${mod}+r" = "mode resize";
+        "${modifier}+r" = "mode resize";
       };
       modes = {
         resize = {
