b179dd086b7d76e8f5ecf1c4eaf635416de686db
sudosubin <sudosubin@gmail.com>
2022-08-27 12:37:30 +0900

    feat: Version up yabai (4.0.1 -> 4.0.2)

--- libraries/nixpkgs/programs/yabai/default.nix
+++ libraries/nixpkgs/programs/yabai/default.nix
@@ -10,13 +10,13 @@ in
 {
   yabai = prev.yabai.overrideDerivation (attrs: rec {
     pname = "yabai";
-    version = "4.0.1";
+    version = "4.0.2";
 
     src = fetchFromGitHub {
       owner = "koekeishiya";
       repo = pname;
       rev = "v${version}";
-      sha256 = "sha256-H1zMg+/VYaijuSDUpO6RAs/KLAAZNxhkfIC6CHk/xoI=";
+      sha256 = "sha256-DXDdjI4kkLcRUNtMoSu7fJ0f3fUty88o5ZS6lJz0cGU=";
     };
 
     nativeBuildInputs = attrs.nativeBuildInputs ++ [ buildSymlinks ];
