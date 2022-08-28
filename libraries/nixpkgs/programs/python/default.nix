b6370d99163b6bf432519d2d78bed4b0699f8d6d
sudosubin <sudosubin@gmail.com>
2022-08-28 14:30:38 +0900

    feat: Update dependencies

--- libraries/nixpkgs/programs/python/default.nix
+++ libraries/nixpkgs/programs/python/default.nix
@@ -4,7 +4,6 @@ final: { lib, fetchzip, stdenv, ... }@prev:
   python310 = prev.python310.override {
     packageOverrides = self: super: {
       debugpy = (import ./debugpy self super);
-      diff-cover = (import ./diff-cover self super);
       pyopenssl = (import ./pyopenssl self super);
     };
   };
