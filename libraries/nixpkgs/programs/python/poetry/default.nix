07699f6d9b96ba5ace2b1d15c53dcbbd03a22fea
sudosubin <sudosubin@gmail.com>
2022-10-11 00:59:11 +0900

    feat: Update dependencies

--- libraries/nixpkgs/programs/python/poetry/default.nix
+++ libraries/nixpkgs/programs/python/poetry/default.nix
@@ -1,14 +1,16 @@
 self: super:
+
 let
-  inherit (super.pkgs) lib stdenvNoCC;
+  inherit (super.pkgs) lib;
+  inherit (super.pkgs.stdenvNoCC.hostPlatform) isAarch64 isDarwin;
 
 in
 super.poetry.overridePythonAttrs (attrs: {
-  preCheck = lib.optionalString (stdenvNoCC.isDarwin && stdenvNoCC.isAarch64) ''
+  preCheck = lib.optionalString (isDarwin && isAarch64) ''
     export no_proxy='*';
   '';
 
-  postCheck = lib.optionalString (stdenvNoCC.isDarwin && stdenvNoCC.isAarch64) ''
+  postCheck = lib.optionalString (isDarwin && isAarch64) ''
     unset no_proxy
   '';
 })
