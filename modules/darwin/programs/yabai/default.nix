76e5515a70a5377410f6271ce0a481a1e6f54a2b
sudosubin <sudosubin@gmail.com>
2022-10-17 02:03:57 +0900

    feat: Update yabai configurations

--- modules/darwin/programs/yabai/default.nix
+++ modules/darwin/programs/yabai/default.nix
@@ -5,12 +5,21 @@
     enable = true;
 
     config = {
-      layout = "bsp";
+      # Global Settings
+      mouse_follows_focus = "on";
+      window_origin_display = "focused";
+      window_topmost = "on";
       window_shadow = "float";
+      auto_balance = "on";
+      mouse_modifier = "ctrl";
+
+      # Space Settings
+      layout = "bsp";
       window_placement = "second_child";
     };
 
     extraConfig = ''
+      yabai -m rule --add app="^System Preferences$" manage="off"
     '';
   };
 }
