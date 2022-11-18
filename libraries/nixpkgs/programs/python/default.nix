54074b4ee187881c00fc0ea3371023852e4c2a76
sudosubin <sudosubin@gmail.com>
2022-11-18 19:04:32 +0900

    feat: darin Home Manager Apps

--- libraries/nixpkgs/programs/python/default.nix
+++ libraries/nixpkgs/programs/python/default.nix
@@ -3,9 +3,7 @@ final: prev:
 {
   python310 = prev.python310.override {
     packageOverrides = self: super: {
-      cherrypy = (import ./cherrypy self super);
-      poetry = (import ./poetry self super);
-      pyopenssl = (import ./pyopenssl self super);
+      # pyopenssl = (import ./pyopenssl self super);
     };
   };
 }
