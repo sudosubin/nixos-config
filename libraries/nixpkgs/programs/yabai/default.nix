081406b44d87847dc4ea2de7ff320bcc70956b63
sudosubin <sudosubin@gmail.com>
2022-12-18 00:38:47 +0900

    feat: Version up yabai (5.0.1 -> 5.0.2)

--- libraries/nixpkgs/programs/yabai/default.nix
+++ libraries/nixpkgs/programs/yabai/default.nix
@@ -7,12 +7,12 @@ in
 {
   yabai = prev.yabai.overrideDerivation (attrs: lib.optionals (isDarwin && isAarch64) rec {
     pname = "yabai";
-    version = "5.0.1";
+    version = "5.0.2";
     name = "${pname}-${version}";
 
     src = fetchzip {
       url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-      sha256 = "sha256-iCx/e3IwJ6YzgEy7wGkNQU/d7gaZd4b/RLwRvRpwVwQ=";
+      sha256 = "sha256-wL6N2+mfFISrOFn4zaCQI+oH6ixwUMRKRi1dAOigBro=";
     };
   });
 }
