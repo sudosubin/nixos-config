e18cdfa72518a46b9998abe277083f0eb6edae5f
sudosubin <sudosubin@gmail.com>
2023-01-09 00:29:50 +0900

    feat: Update dependencies

--- modules/shared/programs/vim/default.nix
+++ modules/shared/programs/vim/default.nix
@@ -4,8 +4,19 @@
   programs.neovim = {
     enable = true;
 
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
     withNodeJs = false;
     withRuby = false;
     withPython3 = false;
+
+    defaultEditor = true;
+    # extraConfig = "";
+    # extraPackages = with pkgs; [];
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+    ];
   };
 }
