0281e36cbeedcb88aeb767e8f0f657324c02bcf5
sudosubin <sudosubin@gmail.com>
2024-06-03 17:33:28 +0900

    fix: Update redis insight download url and update script

--- libraries/nixpkgs/programs/redisinsight/default.nix
+++ libraries/nixpkgs/programs/redisinsight/default.nix
@@ -3,19 +3,19 @@
 let
   sources = {
     "aarch64-darwin" = {
-      url = "https://download.redisinsight.redis.com/latest/RedisInsight-mac-arm64.dmg";
-      sha256 = "10y52nlbiqs7f85r1d83z4y68mbgzs5z7zaiq01spjbxfpdanpch";
+      url = "https://download.redisinsight.redis.com/latest/Redis-Insight-mac-arm64.dmg";
+      sha256 = "102p494bvjnr157q0xyh10wq69kd8yr1c9ph670qyi4pr0gm9gbx";
     };
     "x86_64-darwin" = {
-      url = "https://download.redisinsight.redis.com/latest/RedisInsight-mac-x64.dmg";
-      sha256 = "06r668l4bjx2s3p19n7av8717y05lq90dzss42mnlm7y684zjhxi";
+      url = "https://download.redisinsight.redis.com/latest/Redis-Insight-mac-x64.dmg";
+      sha256 = "1sm17nxw2jy08bp2yivi2ibn3nyh42p09qkj36k4d9srmnpwz8k9";
     };
   };
 
 in
 stdenvNoCC.mkDerivation rec {
   pname = "redisinsight";
-  version = "2.48.0";
+  version = "2.50.0";
 
   src = fetchurl {
     inherit (sources.${stdenvNoCC.hostPlatform.system}) url sha256;
