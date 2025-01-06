d63fc6d09c1300fb4a70683627962fc00464a6d7
sudosubin <sudosubin@gmail.com>
2025-01-07 01:52:42 +0900

    feat: Update fzf to follow gitignore

--- modules/shared/programs/shell/fzf.nix
+++ modules/shared/programs/shell/fzf.nix
@@ -1,8 +1,12 @@
-{ config, pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  fd = lib.getExe' pkgs.fd "fd";
 
+in
 {
-  programs.fzf = {
+  programs.fzf = rec {
     enable = true;
+    defaultCommand = "${fd} --strip-cwd-prefix --exclude .git";
     defaultOptions = [
       "--color=dark"
       "--color=fg:-1,bg:-1,hl:#98c379"
@@ -10,5 +14,6 @@
       "--color=info:#e5c07b,prompt:#98c379,pointer:#56b6c2"
       "--color=marker:#56b6c2,spinner:#e5c07b,header:#56b6c2"
     ];
+    fileWidgetCommand = defaultCommand;
   };
 }
