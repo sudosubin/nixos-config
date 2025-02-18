ead11ebce0d18adc5c18de0e0828f03e0392656e
sudosubin <sudosubin@gmail.com>
2025-02-18 12:41:32 +0900

    feat: Update dependencies

--- libraries/nixpkgs/programs/clop/default.nix
+++ libraries/nixpkgs/programs/clop/default.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "clop";
-  version = "2.8.7";
+  version = "2.9.0";
 
   src = fetchurl {
     url = "https://github.com/FuzzyIdeas/Clop/releases/download/v${version}/Clop-${version}.dmg";
-    sha256 = "1sb5aipccbckkppcg857bmz3xb8dym72zdffjklxbhvx49pzfwhx";
+    sha256 = "0xpip1bn79qvkyq3fzmccqszynm5pyqfna5k6dmmxpdf34da1bbv";
   };
 
   sourceRoot = "Clop.app";
