250ce9450caaa837e0a2de2f3d3f3bc21d4a6b72
sudosubin <sudosubin@gmail.com>
2025-04-09 19:47:55 +0900

    feat: Remove chrome-web-store.deepl-translate, add chrome-web-store.google-translate

--- modules/shared/programs/chromium/default.nix
+++ modules/shared/programs/chromium/default.nix
@@ -6,7 +6,7 @@
     package = pkgs.ungoogled-chromium;
     extensions = [
       pkgs.chrome-web-store._1password-password-manager
-      pkgs.chrome-web-store.deepl-translate
+      pkgs.chrome-web-store.google-translate
       pkgs.chrome-web-store.neutral-face-emoji-tools
       pkgs.chrome-web-store.ublock-origin-lite
     ];
