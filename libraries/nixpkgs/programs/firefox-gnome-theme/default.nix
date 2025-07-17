5343cf619c6405a78a5efa8ae526f8e36a02b907
sudosubin <sudosubin@gmail.com>
2025-07-17 23:05:29 +0900

    feat: Update dependencies

--- libraries/nixpkgs/programs/firefox-gnome-theme/default.nix
+++ libraries/nixpkgs/programs/firefox-gnome-theme/default.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   pname = "firefox-gnome-theme";
-  version = "137";
+  version = "140";
 
   src = fetchurl {
     url = "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/refs/tags/v${version}.zip";
-    sha256 = "00xk30nx6ghl5dz7ms8anpnayfa6lh1blqni8qbmr7ng5qs8wzsw";
+    sha256 = "13ml3sxbwq8n197sh47azkpl1n4jidp6aybqrpnx9l5bb051b9c5";
   };
 
   nativeBuildInputs = [ unzip ];
