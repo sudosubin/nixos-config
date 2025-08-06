2034062d38b3f887fe4583a68cfaa39308bfcb39
sudosubin <sudosubin@gmail.com>
2025-08-06 20:18:53 +0900

    feat: Update nix-activate

--- libraries/nixpkgs/programs/nix-activate/default.nix
+++ libraries/nixpkgs/programs/nix-activate/default.nix
@@ -14,8 +14,8 @@ rustPlatform.buildRustPackage (finalAttrs: {
   src = fetchFromGitHub {
     owner = "sudosubin";
     repo = finalAttrs.pname;
-    rev = "5debfbe";
-    sha256 = "sha256-mSj0xgsYK6QBRA3MAd1L9a91xEkt82EHKs4mQtE+lw0=";
+    rev = "c2336a6";
+    sha256 = "sha256-BiFwkWfvuPf64SX9PTZllOJq0RKJbbh2vlMOIlEzEkI=";
   };
 
   cargoHash = "sha256-xO753cda/cUm/Z6tgC3BC3xRX2gHmTsvjOCITvbxWfU=";
