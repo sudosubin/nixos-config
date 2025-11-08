365d507a8b3b746a009b7dbfaf752328ec64676e
sudosubin <sudosubin@gmail.com>
2025-11-08 23:45:51 +0900

    refactor: Refact redisinsight nix module

--- libraries/nixpkgs/programs/redisinsight/default.nix
+++ libraries/nixpkgs/programs/redisinsight/default.nix
@@ -5,44 +5,40 @@
   _7zz,
 }:
 
-stdenvNoCC.mkDerivation (
-  finalAttrs:
-  let
-    sources = {
-      "aarch64-darwin" = {
-        url = "https://download.redisinsight.redis.com/latest/Redis-Insight-mac-arm64.dmg";
-        sha256 = "0d6bkwgnxyvvlx4lzs783v8crbxrmy98kdsa1l2jbiswbf24gy1f";
-      };
-      "x86_64-darwin" = {
-        url = "https://download.redisinsight.redis.com/latest/Redis-Insight-mac-x64.dmg";
-        sha256 = "1fnhbmlj0d1r3fhq9smq1ygwqzv4hiqd5xndcsxaxkw1mvifsgf3";
-      };
+let
+  inherit (stdenvNoCC) hostPlatform;
+  sources = {
+    aarch64-darwin = fetchurl {
+      url = "https://download.redisinsight.redis.com/latest/Redis-Insight-mac-arm64.dmg";
+      sha256 = "0d6bkwgnxyvvlx4lzs783v8crbxrmy98kdsa1l2jbiswbf24gy1f";
     };
-
-  in
-  {
-    pname = "redisinsight";
-    version = "2.70.1";
-
-    src = fetchurl {
-      inherit (sources.${stdenvNoCC.hostPlatform.system}) url sha256;
+    x86_64-darwin = fetchurl {
+      url = "https://download.redisinsight.redis.com/latest/Redis-Insight-mac-x64.dmg";
+      sha256 = "1fnhbmlj0d1r3fhq9smq1ygwqzv4hiqd5xndcsxaxkw1mvifsgf3";
     };
+  };
 
-    sourceRoot = "Redis Insight.app";
+in
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "redisinsight";
+  version = "2.70.1";
 
-    nativeBuildInputs = [ _7zz ];
+  src = sources.${hostPlatform.system};
 
-    installPhase = ''
-      mkdir -p "$out/Applications/${finalAttrs.sourceRoot}"
-      cp -R . "$out/Applications/${finalAttrs.sourceRoot}"
-    '';
+  sourceRoot = "Redis Insight.app";
 
-    meta = {
-      description = "GUI for streamlined Redis application development";
-      homepage = "https://redis.com/redis-enterprise/redis-insight/";
-      license = lib.licenses.unfree;
-      maintainers = with lib.maintainers; [ sudosubin ];
-      platforms = builtins.attrNames sources;
-    };
-  }
-)
+  nativeBuildInputs = [ _7zz ];
+
+  installPhase = ''
+    mkdir -p "$out/Applications/${finalAttrs.sourceRoot}"
+    cp -R . "$out/Applications/${finalAttrs.sourceRoot}"
+  '';
+
+  meta = {
+    description = "GUI for streamlined Redis application development";
+    homepage = "https://redis.com/redis-enterprise/redis-insight/";
+    license = lib.licenses.unfree;
+    maintainers = with lib.maintainers; [ sudosubin ];
+    platforms = builtins.attrNames sources;
+  };
+})
