50c5b6554bbb479616416e00c2fc0725f8717d85
sudosubin <sudosubin@gmail.com>
2026-02-05 13:41:25 +0900

    feat: Remove gemini-cli and gemini-mcp-tool

--- modules/shared/programs/ai/default.nix
+++ modules/shared/programs/ai/default.nix
@@ -43,39 +43,6 @@
         defaultMode = "bypassPermissions";
       };
     };
-
-    mcpServers = {
-      gemini-cli = {
-        type = "stdio";
-        command = lib.getExe pkgs.gemini-mcp-tool;
-        args = [ ];
-      };
-    };
-  };
-
-  programs.gemini-cli = {
-    enable = true;
-    package = pkgs.gemini-cli-bin;
-
-    # https://github.com/google-gemini/gemini-cli/blob/main/docs/cli/configuration.md
-    settings = {
-      general = {
-        checkpointing.enabled = true;
-        disableAutoUpdate = true;
-        disableUpdateNag = true;
-      };
-      privacy = {
-        usageStatisticsEnabled = false;
-      };
-      security = {
-        auth.selectedType = "oauth-personal";
-      };
-      ui = {
-        hideBanner = true;
-        hideTips = true;
-        theme = "GitHub";
-      };
-    };
   };
 
   programs.pi = {
