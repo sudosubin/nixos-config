39ce6b2d2fc5f8ee2618a363b6e6912404107f55
sudosubin <sudosubin@gmail.com>
2026-02-15 21:04:25 +0900

    feat: Add bat github-dark theme

--- modules/shared/programs/bat/default.nix
+++ modules/shared/programs/bat/default.nix
@@ -5,9 +5,13 @@
     enable = true;
 
     config = {
-      theme = "base16";
+      theme = "github-dark";
       style = "numbers,changes,header";
       italic-text = "always";
+    };
+
+    themes = {
+      github-dark.src = ./files/github-dark.tmTheme;
     };
   };
 }
