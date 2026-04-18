d0a6779227dd20d3225a952a43754b27567e6c40
sudosubin <sudosubin@gmail.com>
2026-04-18 18:35:05 +0900

    fix: Update sqlit process worker configuration

--- modules/shared/programs/sqlit/default.nix
+++ modules/shared/programs/sqlit/default.nix
@@ -6,6 +6,8 @@
     configDir = ".config/sqlit";
     settings = {
       allow_plaintext_credentials = true;
+      process_worker = false;
+      process_worker_warm_on_idle = false;
       theme = "textual-ansi";
     };
   };
